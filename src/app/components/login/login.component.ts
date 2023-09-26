0b424d274dc407f6d443e9c32655065ec90c26ad
Manuel Rivas Gauna <manuelrivasgauna@cofarsur.com>
2023-09-26 09:26:35 -0300

    cambio en headers

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -15,7 +15,10 @@ export class LoginComponent implements OnInit {
 
   onSubmit() {
     const urlAPI = 'https://9213-200-126-197-195.ngrok.io/apiExplenderAlert/Login/iniciar_sesion';
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const headers = new HttpHeaders({ 
+      'Content-Type': 'application/json',
+      'ngrok-skip-browser-warning': 'true' // Agregar el encabezado ngrok-skip-browser-warning
+    });
 
     const requestBody = {
       username: this.loginForm.username,
